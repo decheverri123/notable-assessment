da9bcceb73c8aa2d1cb72500d5ba0fb85236a6aa
Danny Echeverri <decheverri123@gmail.com>
2022-08-15 19:05:50 -0400

    server setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Axios from "axios";
+
+interface Blog {
+  id: Number;
+  title: String;
+  body: String;
+  author: String;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+  useEffect(() => {
+    const url = "http://localhost:8000/blogs";
+    Axios.get(url).then((res) => {
+      setBlogs(res.data);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello</h1>
+      {blogs &&
+        blogs.map((blog) => {
+          return (
+            <div>
+              <h2>{blog.title}</h2>
+              <h3>{blog.author}</h3>
+              <p>{blog.body}</p>
+            </div>
+          );
+        })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
